20be68d1a4f9e46b72639a3383fd0fef19868cd3
Andrew McCrary <andrew_mccrary@outlook.com>
2023-07-15 22:00:03 -0400

    Swagger

---
+++ src/MILF/MILF.Web/Controllers/TestController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MILF.Web.Controllers;
+
+[ApiController]
+[Route("Test")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public int GetNum() => 5;
+}
--- src/MILF/MILF.Web/Startup.cs
+++ src/MILF/MILF.Web/Startup.cs
@@ -17,23 +17,31 @@ public class Startup
 
     public void ConfigureServices(IServiceCollection services)
     {
+        services.AddControllers();
 
+        services.AddEndpointsApiExplorer();
+
+        services.AddSwaggerGen();
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
         if (env.IsDevelopment())
         {
-            // Use swagger
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
 
         app.UseStaticFiles();
-        app.UseAuthentication();
-        app.UseAuthorization();
+        //app.UseAuthentication();
+        //app.UseAuthorization();
+        app.UseRouting();
 
         app.UseEndpoints(endpoints =>
         {
-            endpoints.MapControllers();
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "api/{controller}/{action=Index}/{id?}");
         });
     }
 }

02d4ab67cd3ef4336d7d4c5a7cd48a273f4bc24f
Andrew McCrary <andrew_mccrary@outlook.com>
2023-07-15 21:30:43 -0400

    re init angular project

--- src/MILF/MILF.Business/GlobalUsings.cs
+++ src/MILF/MILF.Business/GlobalUsings.cs
@@ -1,4 +1,1 @@
 ﻿global using System;
-using System.Net.Sockets;
-
-docker pull mcr.microsoft.com/mssql/server
--- src/MILF/MILF.Web/ClientApp/src/app/app.component.spec.ts
+++ src/MILF/MILF.Web/ClientApp/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'milf-client'`, () => {
+  it(`should have as title 'milf-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('milf-client');
+    expect(app.title).toEqual('milf-web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('milf-client app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('milf-web app is running!');
   });
 });

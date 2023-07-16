3ebae8b9a53cd95b5a4fb267bb36bdf0c82f1edb
Andrew McCrary <andrew_mccrary@outlook.com>
2023-07-15 23:44:20 -0400

    Fix angular

--- src/MILF/MILF.Web/ClientApp/src/app/app.component.ts
+++ src/MILF/MILF.Web/ClientApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'milf-web';
+
+  public menuItems: MenuItem[] = [
+    {
+      label: 'home',
+      url: '/',
+      tabindex: '0',
+    }
+  ]
+
 }
--- src/MILF/MILF.Web/ClientApp/src/app/app.module.ts
+++ src/MILF/MILF.Web/ClientApp/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenubarModule } from 'primeng/menubar';
+import { NewsComponent } from './news/news.component';
+import { SocialComponent } from './social/social.component';
+import { MapComponent } from './map/map.component';
+import { TutorialsComponent } from './tutorials/tutorials.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsComponent,
+    SocialComponent,
+    MapComponent,
+    TutorialsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MenubarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

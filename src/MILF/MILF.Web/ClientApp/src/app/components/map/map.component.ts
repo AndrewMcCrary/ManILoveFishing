605f419851be66a5456a62046c67488bc42b6098
Bxcrnr <40781446+Bxcrnr@users.noreply.github.com>
2023-07-29 20:51:14 -0400

    Map api (#21)
    * Updated UI with all tabs and skeleton for Tutorials page.
    
    * Added Map Functionality
    
    I got our map to work
    
    * Revert "Added Map Functionality"
    
    This reverts commit 96f10442500e4753beef0a056de9940c1c782cc8.
    
    * Revert "Revert "Added Map Functionality""
    
    This reverts commit c6d46297279f36e3d444d69e886f1b6c5a6ac759.
    
    * news
    
    ---------
    
    Co-authored-by: Matthew McGowan <mc.mcgowan03@gmail.com>

--- src/MILF/MILF.Web/ClientApp/src/app/components/map/map.component.ts
+++ src/MILF/MILF.Web/ClientApp/src/app/components/map/map.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+/// <reference types="@types/googlemaps" />
+
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent {
+export class MapComponent implements AfterViewInit {
+  @ViewChild('mapContainer') mapContainer!: ElementRef;
 
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    const mapOptions: google.maps.MapOptions = {
+      center: { lat: 40.712776, lng: -74.005974 }, // Replace with your desired latitude and longitude
+      zoom: 12, // Set the initial zoom level of the map
+    };
+
+    new google.maps.Map(this.mapContainer.nativeElement, mapOptions);
+  }
 }
